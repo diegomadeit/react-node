dc65b7f83eb37bb08fcea7ad21a597e27c1357b3
Borys Generalov <bgener@gmail.com>
2017-03-30 15:11:42 +0200

    added soap client

--- README.md
+++ README.md
@@ -1,1 +1,19 @@
-React and Node app
+## Introduction
+This is a sample implementation of API Gateway in node.js. The web app (reactjs) sends REST requests to the Gateway, which in turn transforms and forwards them to the SOAP endpoints and reponses back with a json.
+
+## Features
+* NodeJs
+* Express
+* Soap
+* ReactJs
+
+## How to use?
+1. start a server
+
+    cd \src
+    npm start
+
+2. direct your browser to
+
+    http://localhost:3000
+
--- src/server/api/permissiveAction.js
+++ src/server/api/permissiveAction.js
@@ -1,7 +1,19 @@
 (() => {
     const app = require('../app');
+    const soap = require('soap');
 
     app.get('/api/permissiveaction', function (req, res) {
-        res.send('Hello from APIv1 permissive action route.');
+        res.send('Hello from API v1 permissive action handler.');
+
+        var url = process.env.QUERY_ENDPOINT_URL;
+        var args = { name: 'value' };
+        soap.createClient(url, function (err, client) {
+            client.Execute(args, function (err, result) {
+                if (err) {
+                    console.error(err);
+                }
+                console.log(result);
+            });
+        });
     });
 })();
